e4bf1f6339801edef557ed35f4b42e4daf8337f1
David Puelz <dpuelz@gmail.com>
2023-01-25 13:16:17 -0600

    FRT example code

---
+++ code/FRTexample.R
@@ -0,0 +1,24 @@
+##### FRT test
+
+# my function
+DiM_function = function(Z,Y){
+  DiM = mean(Y[which(Z==1)]) - mean(Y[which(Z==0)])
+  return(DiM)
+}
+
+# simulating data and running an FRT
+set.seed(1)
+Yobs = rnorm(20)
+Zobs = rbinom(20,1,0.5)
+Yobs[Zobs==1] = Yobs[Zobs==1] + 5 #additive treatment effect of 0.1
+Tobs = DiM_function(Zobs,Yobs)
+
+Tprime = c()
+for(ii in 1:1000){
+  Zprime = rbinom(20,1,0.5)
+  Tprime = c(Tprime,DiM_function(Zprime,Yobs)) # Yobs stays fixed because null hypothesis tells me so!
+}
+hist(Tprime)
+abline(v=Tobs,col='orange',lwd=3)
+FRTpvalue = mean(Tprime>=Tobs)
+
