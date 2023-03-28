c5c42994a51bc24b749404ceb0f6ae8b1f7651e4
David Puelz <dpuelz@gmail.com>
2023-03-28 10:09:55 -0500

    remove old code

--- code/student_randomizer.R
+++
@@ -1,9 +0,0 @@
-## project presentation script
-
-set.seed(1)
-
-students = read.csv("../data/students_2022.csv",header = F)
-ord = sample(1:nrow(students))
-
-final = cbind(noquote(c(rep('JPE',6),rep('Walmart',6))),students[ord,])
-noquote(final)
